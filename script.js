b314f735db123726c05ff4d8688749c2fd02ed08
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-15 15:31:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+window.addEventListener("resize" , getDimension);
+
+getDimension();
+const body = document.querySelector('body');
+function getDimension(){
+	let height = window.innerHeight;
+	let width = window.innerWidth;
+
+	body.innerText = `WIdth: ${width} and Height: ${height}`;
+}
